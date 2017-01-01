8d6afe9eda1dbd17926d61b77c64c08030a320e0
jskorups <skorupski.jakub@gmail.com>
2017-01-01 22:43:18 +0100

    Creating script

--- polmaker.js
+++ polmaker.js
@@ -2,45 +2,20 @@
  * Created by Kuba on 12/14/2016.
  */
 
-
-
-
-/*
-function fixing () {
-
-
-    //przechowuje wklejone napisy
-
-    var str = document.getElementById("comment1").value;
-    var fixed = str.replace(/d/g, "KKK");
-
-    console.log(fixed);
-
-    var proper = document.getElementById("comment2");
-    proper.value = fixed;
-
-  /*  var i;
-    for (i = 0; i < str.length; i++)  {
-
-        var string = str[i];
-        string.replace(/³/, "l");
-        console.log(string);
-
-        }
-*/
-//}
-
 function fixing () {
 
 
     var str = document.getElementById("comment1").value;
     var mapObj = {
-        d: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
-        e: "DDDDDDDDDDDDDDDDDDDDDDD",
+        æ: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+        "¹": "DDDDDDDDDDDDDDDDDDDDDDD",
         g: "GGGGGGGGGGGGGGGGGGGGGGGGG"
     };
-    str = str.replace(/d|e|g/gi, function (matched) {
+    str = str.replace(/æ|¹|g/gi, function (matched) {
         return mapObj[matched];
-        console.log(mapObj[matched]);
     });
+
+    var str2 = document.getElementById("comment2");
+    str2.innerHTML = str;
+
 }
